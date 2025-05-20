5e693b6dce1ed4bfb94b9350a50966b27deb9e58
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2025-05-20 20:56:00 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Bye'
+    expected_text = 'Hi GFG Workshop'
     assert expected_text.encode() in response.data
 
 
